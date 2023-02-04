6bb78256074556950a10839059ad095f8f2637ca
Tiago <70555750+tiagoluis86@users.noreply.github.com>
2023-02-04 17:17:14 -0300

    ADD permissão à requisições CORS

--- CEPManagerBack/src/main/java/com/cepmanager/tiagocustodio/controller/EnderecoController.java
+++ CEPManagerBack/src/main/java/com/cepmanager/tiagocustodio/controller/EnderecoController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 // Controlador REST para a entidade "Endereço" e tem métodos para realizar operações CRUD
 // Ela usa um objeto EnderecoService para executar as ações requisitadas.
+@CrossOrigin(origins = {"http://localhost:8090", "http://127.0.0.1:5500"})
 @RestController
 @RequestMapping("/enderecos")
 public class EnderecoController {
